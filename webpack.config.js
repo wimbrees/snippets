cbc78db6ed5ee97b5c58bf8b9acbb1851b858708
wimbrees <wimbrees@users.noreply.github.com>
2016-05-14 14:19:00 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,24 @@ module.exports = {
     entry: [
         'webpack-dev-server/client?http://127.0.0.1:8080/',
         'webpack/hot/only-dev-server',
+        // /src/index.js
         './src'
     ],
     output: {
         path: path.join(__dirname, 'public'),
+        // <script src="bundle.js"></script>
         filename: 'bundle.js'
     },
     resolve: {
-        modulesDirectories: ['node_modules', 'src']
+        // No need to write './' when importing from /src
+        modulesDirectories: ['node_modules', 'src'],
+        // No need to write extensions when importing .jsx files
+        extensions: ['', '.js', '.jsx']
     },
     module: {
         loaders: [
             {
+                // Loads for .js and .jsx files
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015']
@@ -29,3 +35,4 @@ module.exports = {
         new webpack.NoErrorsPlugin()
     ]
 };
+
